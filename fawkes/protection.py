a67d5a10bc68abbabacce17e3ac8f5db28bc655a
m1337v <xxmichaelsxx@googlemail.com>
2023-12-25 13:58:36 -0600

    Update protection.py

--- fawkes/protection.py
+++ fawkes/protection.py
@@ -23,7 +23,7 @@ from differentiator import FawkesMaskGeneration
 from utils import init_gpu, dump_image, reverse_process_cloaked, \
     Faces, filter_image_paths, load_extractor
 
-from fawkes.align_face import aligner
+from align_face import aligner
 
 
 def generate_cloak_images(protector, image_X, target_emb=None):
